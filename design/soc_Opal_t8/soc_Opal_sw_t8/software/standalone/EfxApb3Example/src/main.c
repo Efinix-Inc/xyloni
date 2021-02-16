d24991b250d18f32684786f4bb184084069df99e
HansonChung <59546685+HansonChung@users.noreply.github.com>
2021-02-16 09:53:35 +0800

    Update main.c
    updated for Xyloni

--- design/soc_Opal_t8/soc_Opal_sw_t8/software/standalone/EfxApb3Example/src/main.c
+++ design/soc_Opal_t8/soc_Opal_sw_t8/software/standalone/EfxApb3Example/src/main.c
@@ -12,6 +12,10 @@
 #endif
 
 void main() {
+
+    int gpio_read;
+    int led_check;
+    
     struct example_apb3_ctrl_reg cfg={0};
     bsp_init();
 
@@ -19,12 +23,22 @@ void main() {
     gpio_setOutput(BSP_LED_GPIO, 0x00000000);
 
     uart_writeStr(BSP_UART_TERMINAL, "Opal Soc T8: Example Design\n\r");
-    cfg.exampleLED1 = 1;
-    cfg.exampleLED2 = 0; 
+    cfg.exampleLED1 = 1;                //please connect to LED2
+    cfg.exampleLED2 = 0;                //please connect to LED3
     example_register_write(&cfg);
-
+    
+    gpio_read = gpio_getOutput(BSP_LED_GPIO);
+    
     while(1){
-        gpio_setOutput(BSP_LED_GPIO, gpio_getOutput(BSP_LED_GPIO) ^ BSP_LED_MASK);
+        
+        led_check = cfg.exampleLED1 & 0x01;
+        led_check |= (cfg.exampleLED2 & 0x01) <<1;
+        
+        if((example_register_read() & 0x03) ==  led_check))
+        {
+            gpio_setOutput(BSP_LED_GPIO, gpio_read ^ BSP_LED_MASK);
+            gpio_read = ~gpio_read;
+        }
     	cfg.exampleLED1 = ~cfg.exampleLED1;
     	cfg.exampleLED2 = ~cfg.exampleLED2; 
     	example_register_write(&cfg);
